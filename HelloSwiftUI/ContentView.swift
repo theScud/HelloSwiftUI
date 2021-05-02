29ab895a5da48e8e3495fcd035d466137e27feb9
Sudeepkini <sudeep.kini@dunzo.in>
2021-05-02 15:57:17 +0530

    Created a basic splash screen for the app with some test, a background image and  navigation link

--- HelloSwiftUI/ContentView.swift
+++ HelloSwiftUI/ContentView.swift
@@ -7,15 +7,56 @@
 
 import SwiftUI
 
+enum AppScreen {
+    case landing
+    case main
+    case detail
+}
+
 struct ContentView: View {
+    @State private var activeScreen :AppScreen = .landing
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+
+                Image("testimage").resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+                VStack {
+                    Spacer()
+                    Text("Hello, world!")
+                        .padding()
+                        .font(.largeTitle)
+                    Text("this is testabdbak bsdbakjbdkabksdbkjabdjkaksd labdk ")
+                        .padding()
+                        .font(.body)
+                        .multilineTextAlignment(.center)
+                        .frame(width: 250, height: .none, alignment: .center)
+                    Spacer()
+                    NavigationLink(destination: EmptyView()) {
+                        HStack {
+                            Text("Let's Go").font(.largeTitle)
+                            Image(systemName: "chevron.right").scaleEffect(1.5)
+                        }
+                        .padding(.leading, 20)
+                        .padding(.trailing, 20)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10.0)
+                                .stroke(lineWidth: 2.0)
+                        )
+
+                    }
+
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+      Group {
         ContentView()
+      }
     }
 }
