f6eb1712ac480e60b36913d269683b0f3ed62e8b
Sudeepkini <sudeep.kini@dunzo.in>
2021-05-02 10:53:13 +0530

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by Sudeep kini on 02/05/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
